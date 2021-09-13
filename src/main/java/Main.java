ae6e87c0d0072cd83fbc7544be0a63fb69ccfbad
Kaylee Laughner <kayleelaughner@knights.ucf.edu>
2021-09-12 21:56:15 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Kaylee Laughner
+ */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter a noun:");
+        Scanner input = new Scanner(System.in);
+        String noun = input.nextLine();
+
+        System.out.println("Enter a verb:");
+        Scanner input2 = new Scanner(System.in);
+        String verb = input2.nextLine();
+
+        System.out.println("Enter an adjective:");
+        Scanner input3 = new Scanner(System.in);
+        String adj = input3.nextLine();
+
+        System.out.println("Enter an adverb:");
+        Scanner input4 = new Scanner(System.in);
+        String adverb = input4.nextLine();
+
+        System.out.println("Do you " + verb + " your " + adj + " " + noun + " " + adverb + "? That's hilarious!");
+    }
+}
